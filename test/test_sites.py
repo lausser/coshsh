e2185bc3c27cd4963f673b057856e1db809679eb
Gerhard Lausser <gerhard.lausser@consol.de>
2012-07-01 11:39:05 +0200

    add datasource init test case

--- test/test_sites.py
+++ test/test_sites.py
@@ -8,7 +8,7 @@ sys.path.append("..")
 sys.path.append("../coshsh")
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'sites', 'default', 'classes'))
 from generator import Generator
-
+from datasource import Datasource
 
 class CoshshTest(unittest.TestCase):
     def setUp(self):
@@ -17,7 +17,7 @@ class CoshshTest(unittest.TestCase):
         self.generator = Generator()
 
 
-    def test_create(self):
+    def test_create_site(self):
         self.assert_(self.config._sections != {})
         print self.config._sections
         print [section for section in self.config.sections() if section.startswith('site_')]
@@ -29,6 +29,10 @@ class CoshshTest(unittest.TestCase):
         self.assert_(self.generator.sites['test1'].name == 'test1')
         print self.generator.sites['test1'].datasources
 
+    def test_create_datasource(self):
+        cfg = self.config.items("datasource_SIMPLESAMPLE")
+        ds = Datasource(**dict(cfg))
+        print ds
 
 if __name__ == '__main__':
     unittest.main()
