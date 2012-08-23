e3bb007d065c7add1d4f7746dcc9f3a400ce6764
Gerhard Lausser <gerhard.lausser@consol.de>
2012-08-24 01:00:58 +0200

    fix coshsh-create-template-tree and add a testcase

---
+++ test/test_bin.py
@@ -0,0 +1,54 @@
+import unittest
+import os
+import sys
+import shutil
+import string
+from optparse import OptionParser
+import ConfigParser
+import logging
+import subprocess
+
+
+sys.dont_write_bytecode = True
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath(os.path.join("..", "coshsh")))
+
+from generator import Generator
+from datasource import Datasource
+from application import Application
+
+class CoshshTest(unittest.TestCase):
+    def print_header(self):
+        print "#" * 80 + "\n" + "#" + " " * 78 + "#"
+        print "#" + string.center(self.id(), 78) + "#"
+        print "#" + " " * 78 + "#\n" + "#" * 80 + "\n"
+
+    def setUp(self):
+        shutil.rmtree("./var/objects/test1", True)
+        os.makedirs("./var/objects/test1")
+        self.config = ConfigParser.ConfigParser()
+        self.config.read('etc/coshsh.cfg')
+        self.generator = Generator()
+
+    def tearDown(self):
+        #shutil.rmtree("./var/objects/test1", True)
+        pass 
+
+
+    def test_create_template_tree(self):
+        self.print_header()
+        os.makedirs("./var/objects/test1/static")
+        self.assert_(os.path.exists("var/objects/test1/static"))
+        self.assert_(os.path.exists("../bin/coshsh-create-template-tree"))
+        #subprocess.call(["../bin/coshsh-create-template-tree", "--cookbook", "etc/coshsh.cfg", "--recipe", "test4", "--template", "os_windows_fs"], shell=True)
+        subprocess.call("../bin/coshsh-create-template-tree --cookbook etc/coshsh.cfg --recipe test4 --template os_windows_fs", shell=True)
+        self.assert_(os.path.exists("var/objects/test1/static/service_templates"))
+        self.assert_(os.path.exists("var/objects/test1/static/service_templates/os_windows_fs.cfg"))
+        self.assert_(os.path.exists("var/objects/test1/static/service_templates/os_windows.cfg"))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
