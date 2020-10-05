916964cb07a2e23436e30fd4c785367e1d4a4ea3
Gerhard Lausser <gerhard.lausser@consol.de>
2020-10-05 20:29:41 +0200

    fix a bug in pushgateway (failed when no username was set)

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class install_data(_install_data):
 
 
 setup(name='coshsh',
-      version='6.6.2.1',
+      version='6.6.2.2',
       description='Coshsh - config generator for monitoring systems',
       url='http://github.com/lausser/coshsh',
       author='Gerhard Lausser',
