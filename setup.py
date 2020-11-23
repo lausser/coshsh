68849d250f0604b2cdfffc0b8fdbed0254e684e8
Gerhard Lausser <gerhard.lausser@consol.de>
2020-11-23 15:21:09 +0100

    changelog

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class install_data(_install_data):
 
 
 setup(name='coshsh',
-      version='6.6.2.3',
+      version='6.6.2.4',
       description='Coshsh - config generator for monitoring systems',
       url='http://github.com/lausser/coshsh',
       author='Gerhard Lausser',
