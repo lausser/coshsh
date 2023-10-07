6d917f108bfa24b3c82e4a7f39ccfe02c4e5e961
Gerhard Lausser <lausser@yahoo.com>
2023-10-07 20:10:38 +0200

    increate version number of this scheissdreck

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class install_data(_install_data):
 
 
 setup(name='coshsh',
-      version='10.0',
+      version='10.0.0.2',
       setup_requires=['wheel'],
       description='Coshsh - config generator for monitoring systems',
       long_description=open('README.md').read(),
