5dd53f5930120d948d87cd19c7537e015b1b0d0b
Gerhard Lausser <gerhard.lausser@consol.de>
2026-03-18 14:59:57 +0100

    cleanup the sdist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import glob
 import os
 
 setup(name='coshsh',
-      version='12.2.1',
+      version='12.2.1.1',
       python_requires='>=3.8',
       description='Coshsh - config generator for monitoring systems',
       long_description=open('README.md').read(),
